f27641c54e8682ad69a364871370768298ed16a3
Victor Wåhlstrand Skärström <victor.wahlstrand@gmail.com>
2022-11-28 14:53:55 +0100

    Updated api endpoints

--- urls.py
+++ urls.py
@@ -2,33 +2,24 @@ from django.urls import path, include
 from rest_framework import routers
 from . import views
 import diana.utils as utils
-from rest_framework.schemas import get_schema_view
-from django.views.generic import TemplateView
+
 
 router = routers.DefaultRouter()
+endpoint = utils.build_app_endpoint("rwanda")
+documentation = utils.build_app_api_documentation("rwanda", endpoint)
 
-router.register(r'api/geojson/place', views.PlaceOfInterestGeoViewSet, basename='places as geojson')
-router.register(r'api/image', views.IIIFImageViewSet, basename='image')
+router.register(rf'{endpoint}/geojson/place', views.PlaceOfInterestGeoViewSet, basename='places as geojson')
+router.register(rf'{endpoint}/image', views.IIIFImageViewSet, basename='image')
 
 urlpatterns = [
     path('', include(router.urls)),
 
     # Automatically generated views
-    *utils.get_model_urls('rwanda', 'api', exclude=['placeofinterest', 'image']),
+    *utils.get_model_urls('rwanda', endpoint, 
+        exclude=['placeofinterest', 'image', 
+            'image_authors', 'image_informants', 
+            'name_languages', 'name_informants', 
+            'text_authors', 'text_informants']),
+    *documentation
 
-    path('api/schema/', 
-        get_schema_view(
-            title="Rwanda",
-            description="Schema for the Rwanda API at the Centre for Digital Humanities",
-            version="1.0.0",
-            urlconf="apps.rwanda.urls"
-        ), 
-        name='openapi-schema'
-    ),
-    path('api/documentation/', 
-        TemplateView.as_view(
-            template_name='templates/redoc.html',
-            extra_context={'schema_url':'openapi-schema'}
-        ), 
-        name='redoc-ui'),
 ]
