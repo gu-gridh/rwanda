1222ef04fca973937af9e9b694139ea1adf23a36
AramKarimi <aram.karimi@gu.se>
2023-10-31 16:02:11 +0100

    upload file to document model added

--- models.py
+++ models.py
@@ -3,6 +3,8 @@ from django.contrib.gis.db import models
 import diana.abstract.models as abstract
 import diana.abstract.mixins as mixins
 from django.utils.translation import gettext_lazy as _
+from diana.storages import OriginalFileStorage
+from diana.abstract.models import get_original_path
 
 AGE_CHOICES = (
     ('O', 'Old'),
@@ -123,6 +125,7 @@ class Document(abstract.AbstractBaseModel):
 
     title = models.CharField(max_length=1024, null=True, blank=True, verbose_name=_("document title"))
     place_of_interest   = models.ForeignKey(PlaceOfInterest, null=True, blank=True, on_delete=models.CASCADE, related_name="places")
+    filename = models.FieldFile(null=True, blank=True, storage=OriginalFileStorage, upload_to=get_original_path, verbose_name=_("file"))
     text = models.TextField(null=True, blank=True, verbose_name=_("document text"))
     authors = models.ManyToManyField(Author, blank=True, related_name="document")
     informants = models.ManyToManyField(Informant, blank=True, related_name="document", verbose_name=_("document informants"), help_text=_("List of informants attesting to the document."))
